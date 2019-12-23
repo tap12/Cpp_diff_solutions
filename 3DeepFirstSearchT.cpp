1f1b2c64bb91b010a79de0ed36fb8329fe33e239
adamkoz1 <adamkoz11@gmail.com>
2019-12-23 14:48:17 +0100

    working on core by adding cointainers from std

--- 3DeepFirstSearchT.cpp
+++ 3DeepFirstSearchT.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <algorithm>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
 const int MAX_N = 200; // maksymalna ilość wierzchołków w grafie
 const int MAX_T = 100; // max number of testcases
+
+struct TNode
+{
+    int node_number; // Edge Number
+    bool visited;    // is arleady visited or not yet?
+};
+
+struct TGraph
+{
+    TNode Nodes[5];
+};
+
+stack<int> Output; //helper //rename
+vector<int> Output2; //rename
 
 void dfs();
 
@@ -49,7 +65,8 @@ int main()
     cout << N << ": number of Nodes,\n";
     cout << E << ": number of Edges,\n";
     cout << "0 1 0 2 0 3 2 4"
-         << ": given Adjacency list.\n" << "and as result (symetric) Adjacency Matrix below:";
+         << ": given Adjacency Input.\n"
+         << "and as result (symetric) Adjacency Matrix below:";
 
     //Let print Adjacency Matrix
     cout << endl;
@@ -58,5 +75,35 @@ int main()
         for (int j = 0; j < N; j++)
             cout << (int)A_matrix[i][j] << " ";
         cout << endl;
+    }
+
+    cout << "\n\nBut best solution will be Adjacency list, as shown below:\n";
+
+    for (int i = 0; i < E; i++)
+        cout << (int)input_A_list[i][0] << " " << (int)input_A_list[i][1] << endl;
+    cout << endl;
+
+    TGraph myGraf;
+    //let fill all Nodes as not yet visited.
+    for (int i = 0; i < N; i++)
+    {
+        myGraf.Nodes[i].node_number = i;
+        myGraf.Nodes[i].visited = false;
+    }
+    // Output.push(i);
+
+    cout << "\nAnd finally it's time for the Output:\n";
+    for (int i = 0; i < N; i++)
+        Output.push(i);
+
+    // for (auto i = Output.begin(); i != Output.end(); ++i)
+    //     std::cout << *i << ' ';
+
+    //Finally print out what stack Output cointains:
+    while (!Output.empty())
+    {
+        int w = Output.top();
+        cout << w << " ";
+        Output.pop();
     }
 }
